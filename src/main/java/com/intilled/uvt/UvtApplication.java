54d45ceadee5120552a969a1bc2d22dd3c0e7d72
Karan Shah <dabrainstabber@gmail.com>
2019-07-05 23:05:13 +0100

    Added file logging and initial console application

--- src/main/java/com/intilled/uvt/UvtApplication.java
+++ src/main/java/com/intilled/uvt/UvtApplication.java
@@ -1,13 +1,23 @@
 package com.intilled.uvt;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class UvtApplication {
+public class UvtApplication implements CommandLineRunner {
+    private static final Logger LOGGER = LoggerFactory.getLogger(UvtApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(UvtApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        LOGGER.info("Received arguments {}", Arrays.toString(args));
+    }
 }
