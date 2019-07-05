4763e62ade41d7580aace57fa1c30fbdeaa7786b
Karan Shah <dabrainstabber@gmail.com>
2019-07-05 19:31:21 +0100

    Minor Refactoring

--- src/main/java/com/intilled/uvt/UvtApplication.java
+++ src/main/java/com/intilled/uvt/UvtApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UvtApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UvtApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UvtApplication.class, args);
+    }
 
 }
