c8f8236e4bc58aec849721443ab5f4342ba6e692
Karan Shah <dabrainstabber@gmail.com>
2019-07-05 19:28:40 +0100

    Initial Commit

---
+++ src/main/java/com/intilled/uvt/UvtApplication.java
@@ -0,0 +1,13 @@
+package com.intilled.uvt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UvtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UvtApplication.class, args);
+	}
+
+}
